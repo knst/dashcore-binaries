10a7e76db0cf8a8ed7fba0a1e56e5ab62f32e937
pasta <pasta@dashboost.org>
2023-06-09 15:45:05 -0500

    add do and do_release as historical ref

---
+++ do.sh
@@ -0,0 +1,20 @@
+
+cd $VERSION
+
+mkdir debug
+mv debug *debug*
+
+sha256sum * > SHA256SUMS
+for f in dashcore-*; do gpg --detach-sign --armour $f; done
+gpg --clear-sign SHA256SUMS
+rm SHA256SUMS
+
+cd debug
+sha256sum * > SHA256SUMS
+for f in dashcore*; do gpg --detach-sign --armour $f; done
+gpg --clear-sign SHA256SUMS
+rm SHA256SUMS
+
+cd ..
+
+
